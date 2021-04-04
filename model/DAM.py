8002c0abc60adafffa6865a2345a29408a21d73a
yali <yaliqin188@gmail.com>
2021-04-04 00:26:20 -0700

    change to get the

--- model/DAM.py
+++ model/DAM.py
@@ -177,7 +177,7 @@ def dam_output(input,SINGLEMODEL,graph,model):
                 break
         question_number = [number]
         all_data,text_data_classified = prepare_q_a_data(question_number,cls_indexs, question_text, answers_text,word_dict,key_words_list)
-        indexs, answers = predict.test_with_model(conf, graph, model, text_data_classified)
+        indexs, answers = predict.test_with_model(conf,  model,graph, text_data_classified)
         print(indexs)
         output = pop_answers(indexs,question_text,question_number,all_data)
     else:
@@ -186,7 +186,7 @@ def dam_output(input,SINGLEMODEL,graph,model):
         questions = input
         q_a_set = build_bilstm_qa(questions, question_text, answers_text)
         text_data_classified = preprocessor.get_sequence_tokens_with_turn(q_a_set, word_dict)
-        indexs, answers = predict.test_with_model(conf, graph, model, text_data_classified)
+        indexs, answers = predict.test_with_model(conf,  model, graph,text_data_classified)
         answer_data = q_a_set[indexs]
         this_answer = answer_data.split('\t')[-1]
         print(f'answer is: {this_answer}')
